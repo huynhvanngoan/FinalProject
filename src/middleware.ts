5f7459104cb1ceef0be647071e24d26b0ed73976
Quoc Thong <thongnguyennqt@gmail.com>
2024-11-19 23:27:54 +0700

    [update] tour route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,19 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+// import createMiddleware from "next-intl/middleware";
+// import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+// export default createMiddleware(routing);
 
-export const config = {
-    // Match only internationalized pathnames
-    matcher: ["/", "/(vi|en)/:path*"],
-};
+// export const config = {
+//     // Match only internationalized pathnames
+//     matcher: ["/", "/(vi|en)/:path*"],
+// };
+
+const isLoggedIn: boolean = false;
+
+import { NextResponse } from "next/server";
+export function middleware(request: Request) {
+  if (!isLoggedIn) {
+    return NextResponse.redirect("/login");
+  }
+  return NextResponse.next();
+}
